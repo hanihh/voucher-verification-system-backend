535e393a69ad027e75cdcbc9b0274f2b7e9e0249
hani.hawasli <hani.hawasli@5TR90753.theirc.turkey>
2014-09-23 17:39:03 +0300

    Metromic theme added index page added

---
+++ themes/metronic/views/site/index.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!--> 
+<html lang="en"> <!--<![endif]-->
+    <!-- BEGIN HEAD -->
+    <head>
+        <meta charset="utf-8"/>
+        <title>Voucher Verification System</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+        <meta content="" name="description"/>
+        <meta content="" name="author"/>
+        <!-- BEGIN GLOBAL MANDATORY STYLES -->
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
+        <link href="../../theme_files/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../../theme_files/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../../theme_files/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../../theme_files/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
+        <link href="../../theme_files/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css"/>
+        <!-- END GLOBAL MANDATORY STYLES -->
+        <!-- BEGIN PAGE LEVEL PLUGIN STYLES -->
+        <link href="../../theme_files/global/plugins/gritter/css/jquery.gritter.css" rel="stylesheet" type="text/css"/>
+        <link href="../../theme_files/global/plugins/bootstrap-daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css"/>
+        <link href="../../theme_files/global/plugins/fullcalendar/fullcalendar/fullcalendar.css" rel="stylesheet" type="text/css"/>
+        <link href="../../theme_files/global/plugins/jqvmap/jqvmap/jqvmap.css" rel="stylesheet" type="text/css"/>
+        <!-- END PAGE LEVEL PLUGIN STYLES -->
+        <!-- BEGIN PAGE STYLES -->
+        <link href="../../theme_files/admin/pages/css/tasks.css" rel="stylesheet" type="text/css"/>
+        <!-- END PAGE STYLES -->
+        <!-- BEGIN THEME STYLES -->
+        <link href="../../theme_files/global/css/components.css" rel="stylesheet" type="text/css"/>
+        <link href="../../theme_files/global/css/plugins.css" rel="stylesheet" type="text/css"/>
+        <link href="../../theme_files/admin/layout/css/layout.css" rel="stylesheet" type="text/css"/>
+        <link href="../../theme_files/admin/layout/css/themes/default.css" rel="stylesheet" type="text/css" id="style_color"/>
+        <link href="../../theme_files/admin/layout/css/custom.css" rel="stylesheet" type="text/css"/>
+        <!-- END THEME STYLES -->
+        <link rel="shortcut icon" href="favicon.ico"/>
+    </head>
+    <!-- END HEAD -->
+
+    <body>
+        11
+    </body>
